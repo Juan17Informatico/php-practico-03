920a9bd42a3f797b0fa345b3f9bf3c97bb03369b
JuanCM <juancampuzano2356@gmail.com>
2024-03-04 17:29:24 -0500

    Design mas o menos terminado, falta la posibilidad de agregar la altura y ancho del producto

--- reto-seccion15/reto2/app.js
+++ reto-seccion15/reto2/app.js
@@ -15,12 +15,20 @@ const createElementInput = (cant) => {
     for (let i = 1; i <= cant; i++) {
         const input = document.createElement("input");
         const label = document.createElement("label");
+        const div = document.createElement("div");
         input.setAttribute("id", `product${i}`);
         input.setAttribute("name", `product${i}`);
-
+        input.setAttribute("class", `products-input`);
+        label.setAttribute("for", `product${i}`);
         label.innerText = `Producto ${i}`;
 
-        productsDiv.appendChild(label);
-        productsDiv.appendChild(input);
+        productsDiv.append(div);
+        div.appendChild(label);
+        div.appendChild(input);
     }
+
+    // <button type="submit">Enviar Resultado</button>
+
+
+
 };
